ef2166e4e78fbcf37ef27a75359e469f22983814
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2016-03-14 17:50:28 +0800

    Start with vector example

---
+++ WindowsFormsApplication_cpp/WindowsFormsApplication_cpp/DataManager.cpp
@@ -0,0 +1,163 @@
+#include"DataManager.h"
+
+DataManager::DataManager()
+{
+	VectorVariableIndex = 0;
+}
+
+bool DataManager::LoadVectorData()
+{
+	std::fstream fin;
+	//開啟檔案，傳入open函數的參數有兩個，欲開起的檔案名稱，開啟檔案的模式參數(這邊std::ios::in為讀取(輸入)狀態)
+	fin.open(FileName, std::ios::in);
+	//讀取失敗回傳false
+	if (!fin)
+	{
+		return false;
+	}
+	else
+	{
+		//標記當前讀取向量ID
+		int currentLoadVectorID = 0;
+		//定義向量資料暫存變數
+		std::vector<double> tempVectorData;
+		//定義讀取檔案字串暫存變數
+		std::string tempString;
+		//從檔案讀取字串，解析掉向量總數
+		fin >> tempString;
+		
+		//執行讀檔迴圈，並在讀到檔案結尾時結束
+		while (!fin.eof())
+		{
+			//從檔案讀取字串
+			fin >> tempString;
+			//解析到向量標記"V"
+			if (tempString == "V")
+			{
+				if (currentLoadVectorID != 0)
+				{
+					//定義暫存向量資料結構
+					Vector tempVector;
+					//存入向量資料
+					tempVector.Data = tempVectorData;
+					//定義向量變數名稱，依VectorVariableIndex變數作名稱的控管
+					std::string vectorVariableTemp = "$v" + std::to_string(VectorVariableIndex);
+					//存入向量變數名稱
+					tempVector.Name = vectorVariableTemp;
+					//存入向量
+					Vectors.push_back(tempVector);
+					//遞增VectorVariableIndex，以確保變數名稱不重複
+					VectorVariableIndex++;
+					//清除向量資料暫存
+					tempVectorData.clear();
+				}
+				//遞增currentLoadVectorID，標記到當前讀取向量ID
+				currentLoadVectorID++;
+				//從檔案讀取字串，解析掉向量維度
+				fin >> tempString;
+			}
+			else
+			{
+				//讀取向量資料，並將string轉為double
+				double value;
+				value = (double)strtod(tempString.c_str(), NULL);
+				//將向量資料存入暫存
+				tempVectorData.push_back(value);
+			}
+			
+		}
+		//讀入輸入檔案中最後一個向量資訊
+		Vector tempVector;
+		tempVector.Data = tempVectorData;
+		std::string vectorVariableTemp = "$v" + std::to_string(VectorVariableIndex);
+		tempVector.Name = vectorVariableTemp;
+		Vectors.push_back(tempVector);
+		VectorVariableIndex++;
+		//讀取成功回傳false
+		return true;
+	}
+}
+
+std::vector<Vector> DataManager::GetVectors()
+{
+	return Vectors;
+}
+
+bool DataManager::LoadMetrixData()
+{
+	std::fstream fin;
+	//開啟檔案，傳入open函數的參數有兩個，欲開起的檔案名稱，開啟檔案的模式參數(這邊std::ios::in為讀取(輸入)狀態)
+	fin.open(FileName, std::ios::in);
+	//讀取失敗回傳false
+	if (!fin)
+	{
+		return false;
+	}
+	else
+	{
+		//標記當前讀取向量ID
+		int currentLoadVectorID = 0;
+		//定義向量資料暫存變數
+		std::vector<double> tempVectorData;
+		//定義讀取檔案字串暫存變數
+		std::string tempString;
+		//從檔案讀取字串，解析掉向量總數
+		fin >> tempString;
+
+		//執行讀檔迴圈，並在讀到檔案結尾時結束
+		while (!fin.eof())
+		{
+			//從檔案讀取字串
+			fin >> tempString;
+			//解析到向量標記"V"
+			if (tempString == "V")
+			{
+				if (currentLoadVectorID != 0)
+				{
+					//定義暫存向量資料結構
+					Vector tempVector;
+					//存入向量資料
+					tempVector.Data = tempVectorData;
+					//定義向量變數名稱，依VectorVariableIndex變數作名稱的控管
+					std::string vectorVariableTemp = "$v" + std::to_string(VectorVariableIndex);
+					//存入向量變數名稱
+					tempVector.Name = vectorVariableTemp;
+					//存入向量
+					Vectors.push_back(tempVector);
+					//遞增VectorVariableIndex，以確保變數名稱不重複
+					VectorVariableIndex++;
+					//清除向量資料暫存
+					tempVectorData.clear();
+				}
+				//遞增currentLoadVectorID，標記到當前讀取向量ID
+				currentLoadVectorID++;
+				//從檔案讀取字串，解析掉向量維度
+				fin >> tempString;
+			}
+			else
+			{
+				//讀取向量資料，並將string轉為double
+				double value;
+				value = (double)strtod(tempString.c_str(), NULL);
+				//將向量資料存入暫存
+				tempVectorData.push_back(value);
+			}
+
+		}
+		//讀入輸入檔案中最後一個向量資訊
+		Vector tempVector;
+		tempVector.Data = tempVectorData;
+		std::string vectorVariableTemp = "$v" + std::to_string(VectorVariableIndex);
+		tempVector.Name = vectorVariableTemp;
+		Vectors.push_back(tempVector);
+		VectorVariableIndex++;
+		//讀取成功回傳false
+		return true;
+	}
+}
+
+void DataManager::SetFileName(std::string fileName)
+{
+	FileName = fileName;
+}
+
---
+++ WindowsFormsApplication_cpp/WindowsFormsApplication_cpp/DataManager.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<vector>
+#include<iostream>
+#include<fstream>
+#include<string>
+//定義向量資料結構
+struct Vector
+{
+	std::string Name;
+	std::vector<double> Data;
+};
+//定義控管資料class
+class DataManager
+{
+private:
+	//儲存向量資料
+	std::vector<Vector> Vectors;
+	//紀錄向量ID，用於控管
+	int  VectorVariableIndex;
+	//紀錄檔案路徑名稱
+	std::string FileName;
+public:
+	DataManager();
+	//讀取向量資料
+	bool LoadVectorData();
+	//取得向量資料
+	std::vector<Vector> GetVectors();
+	//讀取矩陣資料
+	bool LoadMetrixData();
+	//取得矩陣資料
+	std::vector<Vector> GetMetrixs();
+	//設置檔案路徑名稱
+	void SetFileName(std::string fileName);
+};
