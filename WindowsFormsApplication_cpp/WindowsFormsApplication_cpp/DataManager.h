44017f6b6db49a74ab4261f140c0993ea9696e60
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2016-03-22 11:27:23 +0800

    定義矩陣標頭檔
    目前設定是給只有讀檔部分

--- WindowsFormsApplication_cpp/WindowsFormsApplication_cpp/DataManager.h
+++ WindowsFormsApplication_cpp/WindowsFormsApplication_cpp/DataManager.h
@@ -9,6 +9,18 @@ struct Vector
 	std::string Name;
 	std::vector<double> Data;
 };
+
+//定義向量資料結構
+struct Matrix
+{
+	std::string Name;
+	std::vector<std::vector<double>> Data;
+
+	int colNum;
+	int rowNum;
+
+};
+
 //定義控管資料class
 class DataManager
 {
@@ -17,18 +29,26 @@ private:
 	std::vector<Vector> Vectors;
 	//紀錄向量ID，用於控管
 	int  VectorVariableIndex;
+
+	//儲存矩陣資料
+	std::vector<Matrix> Matrices;
+	//紀錄矩陣ID，用於控管
+	int  MatrixVariableIndex;
+
 	//紀錄檔案路徑名稱
 	std::string FileName;
 public:
 	DataManager();
+
 	//讀取向量資料
 	bool LoadVectorData();
 	//取得向量資料
 	std::vector<Vector> GetVectors();
+
 	//讀取矩陣資料
-	bool LoadMetrixData();
+	bool LoadMatrixData();
 	//取得矩陣資料
-	std::vector<Vector> GetMetrixs();
+	std::vector<Vector> GetMatrices();
 	//設置檔案路徑名稱
 	void SetFileName(std::string fileName);
 };
